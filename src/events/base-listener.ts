372925954b419fa7fc6affb7e63652f3a06f410e
Shaheer Mir <shaheermir0@gmail.com>
2025-01-09 00:44:27 -0500

    Updated

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -7,7 +7,7 @@ interface Event {
 }
 
 export abstract class Listener<T extends Event> {
-  private client: Stan;
+  protected client: Stan;
   protected ackWait = 5 * 1000;
 
   abstract subject: T["subject"];
